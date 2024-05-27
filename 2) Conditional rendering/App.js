c2f1ae6f9e5fc8d2f6854f2d765874f1cdad6c6f
Yaro <92721458+Kefirchik99@users.noreply.github.com>
2024-05-27 20:42:06 +0300

    Create App.js

---
+++ 2) Conditional rendering/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function App() {
+    const [messages, setMessages] = React.useState(["a", "b"])
+
+    return (
+        <div>
+            {messages.length > 0 && <h1>You have {messages.length} unread messages!</h1>}
+        </div>
+    )
+}
